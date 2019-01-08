30072d8db689ffa7c73fa998a19c12cd72a0dc1d
Roland Karlsson <roland.karlsson@247-inc.com>
2019-01-08 15:51:48 +0100

    Show all branches

--- branches.sh
+++ branches.sh
@@ -2,9 +2,14 @@
 
 . `dirname $0`/header
 
+EXTRA=
+if [ $1 = "-a" ]; then
+    EXTRA=-a
+fi
+
 for repo in ${repos}; do
     echo ${HEAD}${repo}${RESET}
     cd ${repo}
-    git branch --color
+    git branch --color ${EXTRA}
     cd ..
 done
