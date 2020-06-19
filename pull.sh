172ee8b4ec2a56771a8ae3ed5a9b666c35494416
Roland Karlsson <roland.karlsson@kivra.com>
2020-06-19 22:08:30 +0200

    Fix pull.sh

--- pull.sh
+++ pull.sh
@@ -5,7 +5,7 @@
 for repo in ${repos}; do
     echo ${HEAD}${repo}${RESET}
     cd ${repo}
-    git pull -p --ff-only | grep -v up-to-date
+    git pull -p --ff-only | grep -v "Already up to date"
     git submodule update
     cd ..
 done
